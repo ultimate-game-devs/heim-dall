c412cfdbe92d125f1905b978a37a506f589e4c5b
Dominik <dasdodomit@gmail.com>
2025-01-15 10:59:56 +0100

    Converting to string

--- Temperatursensor/tempTest.py
+++ Temperatursensor/tempTest.py
@@ -2,6 +2,5 @@ from gpiozero import OutputDevice
 
 temp = OutputDevice(4)
 
-print("values" + temp.values)
-print("value" + temp.value)
+print("value" + str(temp.value))
 print("source" + temp.source)
