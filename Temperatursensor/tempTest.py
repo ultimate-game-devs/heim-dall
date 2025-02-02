35f2cc64b8c8c47c4871fd6917f5e815945f5638
Dominik <dasdodomit@gmail.com>
2025-02-02 11:48:26 +0100

    Using other adafruit_dht (adafruit-circuitpython-dht)

--- Temperatursensor/tempTest.py
+++ Temperatursensor/tempTest.py
@@ -1,14 +1,19 @@
-import Adafruit_DHT
+import adafruit_dht
+import board
 import time
 
-DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 4
+DHT_SENSOR = Adafruit_DHT.DHT11(board.D4)
 
 while True:
-    print(time.time())
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        print(f"Temperature - {temperature} | Humidity - {humidity}")
-    else:
-        print(f"Error: Temperature ({temperature}) or Huminity ({humidity}) None")
+    try:
+        print(time.time())
+        temperature = DHT_SENSOR.temperature
+        humidity = DHT_SENSOR.humidity
+        if humidity is not None and temperature is not None:
+            print(f"Temperature - {temperature}°C | Humidity - {humidity}%")
+        else:
+            print(f"Error: Temperature ({temperature}) or Huminity ({humidity}) None")
+    except Exception as error:
+        DHT_SENSOR.exit()
+        raise error
     time.sleep(3)
