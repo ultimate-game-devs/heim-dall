d247b91d50a5ddded35699c261ecdc5a9de5a7fb
Dominik <dasdodomit@gmail.com>
2025-02-03 09:50:56 +0100

    Basic lcd display output with adafruit-circuitpython-charlcd

---
+++ display.py
@@ -0,0 +1,17 @@
+import board
+import digitalio
+import adafruit_character_lcd.character_lcd as character_lcd
+
+lcd_rs = digitalio.DigitalInOut(board.D22)
+lcd_en = digitalio.DigitalInOut(board.D27)
+lcd_d4 = digitalio.DigitalInOut(board.D17)
+lcd_d5 = digitalio.DigitalInOut(board.D25)
+lcd_d6 = digitalio.DigitalInOut(board.D24)
+lcd_d7 = digitalio.DigitalInOut(board.D23)
+
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+
+lcd.message = "Hello\nCircuitPython"
