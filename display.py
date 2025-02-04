39dc89259569f5d35a6d598482d5a56b54f5f9f2
Dominik <dasdodomit@gmail.com>
2025-02-04 18:36:16 +0100

    Making ruff happy

--- display.py
+++ display.py
@@ -1,6 +1,6 @@
+import adafruit_character_lcd.character_lcd as character_lcd
 import board
 import digitalio
-import adafruit_character_lcd.character_lcd as character_lcd
 
 lcd_rs = digitalio.DigitalInOut(board.D26)
 lcd_en = digitalio.DigitalInOut(board.D19)
@@ -13,6 +13,15 @@ lcd_d4 = digitalio.DigitalInOut(board.D25)
 lcd_columns = 16
 lcd_rows = 2
 
-lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+lcd = character_lcd.Character_LCD_Mono(
+	lcd_rs,
+	lcd_en,
+	lcd_d4,
+	lcd_d5,
+	lcd_d6,
+	lcd_d7,
+	lcd_columns,
+	lcd_rows,
+)
 
-lcd.message = "Hello\nCircuitPython"
+lcd.message = 'Hello\nCircuitPython'
