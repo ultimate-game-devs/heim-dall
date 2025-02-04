947bff16e597539d470d0b34f13cc7cc23a301c9
Dominik <dasdodomit@gmail.com>
2025-02-04 12:44:14 +0100

    Changed the pins again

--- display.py
+++ display.py
@@ -2,12 +2,12 @@ import board
 import digitalio
 import adafruit_character_lcd.character_lcd as character_lcd
 
-lcd_rs = digitalio.DigitalInOut(board.D7)
-lcd_en = digitalio.DigitalInOut(board.D8)
-lcd_d7 = digitalio.DigitalInOut(board.D12)
-lcd_d6 = digitalio.DigitalInOut(board.D11)
-lcd_d5 = digitalio.DigitalInOut(board.D10)
-lcd_d4 = digitalio.DigitalInOut(board.D9)
+lcd_rs = digitalio.DigitalInOut(board.D26)
+lcd_en = digitalio.DigitalInOut(board.D19)
+lcd_d7 = digitalio.DigitalInOut(board.D27)
+lcd_d6 = digitalio.DigitalInOut(board.D22)
+lcd_d5 = digitalio.DigitalInOut(board.D24)
+lcd_d4 = digitalio.DigitalInOut(board.D25)
 
 
 lcd_columns = 16
