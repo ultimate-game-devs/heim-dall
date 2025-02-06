237b90fed121491a7f816dbf1e910cb63c8acaa7
Dominik <dasdodomit@gmail.com>
2025-02-06 10:18:44 +0100

    Added the setup for a motion sensor

--- setUp.py
+++ setUp.py
@@ -1,7 +1,15 @@
 import adafruit_dht
 import board
+import digitalio
 
 
 def dht11(pin_number: int) -> adafruit_dht.DHT11:
 	board_pin = getattr(board, f'D{pin_number}')
 	return adafruit_dht.DHT11(board_pin)
+
+
+def motion(pin_number: int) -> digitalio.DigitalInOut:
+	board_pin = getattr(board, f'D{pin_number}')
+	pir = digitalio.DigitalInOut(board_pin)
+	pir.direction = digitalio.Direction.INPUT
+	return pir
