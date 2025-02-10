a05aae83d2de9f33005be73735788e1cb5fb8594
Dominik <dasdodomit@gmail.com>
2025-02-10 14:51:56 +0100

    Setting up the client with username and password from env. Adopting new callback function from api.v2 defined publish with pass

--- mqtt.py
+++ mqtt.py
@@ -1,5 +1,6 @@
 import os
 import random
+from time import sleep
 
 import paho.mqtt.client as mqtt
 from dotenv import load_dotenv
@@ -7,8 +8,14 @@ from dotenv import load_dotenv
 
 class MQTT:
 	def __init__(self, broker: str, client_name: str) -> None:
+		# Getting username and password from the env
+		load_dotenv()
+		username = os.getenv('MQTT_Username')
+		password = os.getenv('MQTT_Password')
+
 		# Set Up Client
-		self.__client = mqtt.Client(client_name)
+		self.__client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_name)
+		self.__client.username_pw_set(username, password)
 
 		# Binding callbacks
 		self.__client.on_connect = self.__on_connect
@@ -23,23 +30,21 @@ class MQTT:
 		self.__client.disconnect()
 
 	# Callback functions
-	def __on_connect(self, client, userdata, flags, rc) -> None:
-		if rc == 0:
-			print('connected')
-			print(f'client {client}')
-			print(f'userdata {userdata}')
-			print(f'flags {flags}')
-			print(f'rc {rc}')
+	def __on_connect(self, client, userdata, flags, reason_code, properties):
+		if flags.session_present:
+			print(f'Session is resumed or something | flags {flags}')
+		if reason_code == 0:
+			print('Connected to Server')
 		else:
-			print(f'Bad connection | Return code: {rc}')  #
+			print(f'Bad connection | reason_code {reason_code}')
 
-	def __on_message(self, client, userdata, msg) -> None:
+	def __on_message(self, client, userdata, message) -> None:
+		pass
+
+	# Other Functions
+	def publish(self, message):
 		pass
 
 
-load_dotenv()
-
-username = os.getenv('MQTT_Username')
-print(username)
-
 client1 = MQTT('192.168.111.111', f'Python Code {random.randint}')
+sleep(10)
