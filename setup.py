ebef52834d9ff70ec37f68b88a4a89f1221d0ef0
Dominik <dasdodomit@gmail.com>
2025-02-17 16:04:38 +0100

    Removed dots and inits.py's added a dht22 setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+import adafruit_dht
+import board
+import digitalio
+
+
+def dht11(pin_number: int) -> adafruit_dht.DHT11:
+    board_pin = getattr(board, f'D{pin_number}')
+    return adafruit_dht.DHT11(board_pin)
+
+
+def dht22(pin_number: int) -> adafruit_dht.DHT11:
+    board_pin = getattr(board, f'D{pin_number}')
+    return adafruit_dht.DHT22(board_pin)
+
+
+def motion(pin_number: int) -> digitalio.DigitalInOut:
+    board_pin = getattr(board, f'D{pin_number}')
+    pir = digitalio.DigitalInOut(board_pin)
+    pir.direction = digitalio.Direction.INPUT
+    return pir
+
+
+def button(pin_number: int) -> digitalio.DigitalInOut:
+    board_pin = getattr(board, f'D{pin_number}')
+    btn = digitalio.DigitalInOut(board_pin)
+    btn.direction = digitalio.Direction.INPUT
+    btn.pull = digitalio.Pull.UP
+    return btn
--- tests/testTempAndHumid.py
+++ tests/testTempAndHumid.py
@@ -1,11 +1,11 @@
 from time import localtime
 
-from ..inputDevices import DHT11
+from inputDevices import DHT22
 
 while True:
     now = localtime()
     print(f'~~~ {now.tm_hour}:{now.tm_min}:{now.tm_sec} ~~~')
-    dht = DHT11(4)
+    dht = DHT22(4)
     dht_data = dht.get_data()
     if dht_data['temperature'] is not None and dht_data['humidity'] is not None:
         print(f'Temp - {dht_data["temperature"]}°C | Humid - {dht_data["humidity"]}%')
