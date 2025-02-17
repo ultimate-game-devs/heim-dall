f57a5904cb5c9b4c938f39f2f9f7736959e609b4
Dominik <dasdodomit@gmail.com>
2025-02-17 12:32:38 +0100

    Formatting changes

--- Weather.py
+++ Weather.py
@@ -1,47 +1,41 @@
 from typing import List
 
+
 class Coord:
-    def __init__(
-            self, 
-            lon:float, 
-            lat:float
-        ) -> None:
-        self.lon = lon
-        self.lat = lat
+	def __init__(self, lon: float, lat: float) -> None:
+		self.lon = lon
+		self.lat = lat
+
 
 class Weather:
-    def __init__(
-            self, 
-            weather_id:int, 
-            main:str, 
-            description:str, 
-            icon:str
-        ) -> None:
-          self.weather_id = weather_id
-          self.main = main
-          self.description = description
-          self.icon = icon
+	def __init__(self, weather_id: int, main: str, description: str, icon: str) -> None:
+		self.weather_id = weather_id
+		self.main = main
+		self.description = description
+		self.icon = icon
+
 
 class Main:
-    def __init__(
-            self, 
-            temp:float, 
-            feels_like:float, 
-            temp_min:float, 
-            temp_max:float, 
-            pressure:int, 
-            humidity:int, 
-            sea_level:int, 
-            grnd_level:int
-        ) -> None:
-        self.temp = temp
-        self.feels_like = feels_like
-        self.temp_min = temp_min
-        self.temp_max = temp_max
-        self.pressure = pressure
-        self.humidity = humidity
-        self.sea_level = sea_level
-        self.grnd_level = grnd_level
+	def __init__(
+		self,
+		temp: float,
+		feels_like: float,
+		temp_min: float,
+		temp_max: float,
+		pressure: int,
+		humidity: int,
+		sea_level: int,
+		grnd_level: int,
+	) -> None:
+		self.temp = temp
+		self.feels_like = feels_like
+		self.temp_min = temp_min
+		self.temp_max = temp_max
+		self.pressure = pressure
+		self.humidity = humidity
+		self.sea_level = sea_level
+		self.grnd_level = grnd_level
+
 
 class Wind:
 	def __init__(self, speed: float, deg: int, gust: float | None = None) -> None:
@@ -49,42 +43,32 @@ class Wind:
 		self.deg = deg
 		self.gust = gust
 
+
 class Rain:
-    def __init__(
-            self, 
-            one_h:float
-        ) -> None:
-        self.one_h = one_h
+	def __init__(self, one_h: float) -> None:
+		self.one_h = one_h
+
 
 class Snow:
-    def __init__(
-            self, 
-            one_h:float
-        ) -> None:
-        self.one_h = one_h
+	def __init__(self, one_h: float) -> None:
+		self.one_h = one_h
+
 
 class Clouds:
-    # all = cloudiness, but all is a reserved keyword.
-    def __init__(
-            self, 
-            cloudiness:int
-        ) -> None:
-        self.cloudiness = cloudiness
+	def __init__(self, cloudiness: int) -> None:
+		self.cloudiness = cloudiness
+
 
 class Sys:
-    def __init__(
-            self, 
-            sys_type:int, 
-            sys_id:int, 
-            country:str, 
-            sunrise:int, 
-            sunset:int
-        ) -> None:
-        self.sys_type = sys_type
-        self.sys_id = sys_id
-        self.country = country
-        self.sunrise = sunrise
-        self.sunset = sunset
+	def __init__(
+		self, sys_type: int, sys_id: int, country: str, sunrise: int, sunset: int
+	) -> None:
+		self.sys_type = sys_type
+		self.sys_id = sys_id
+		self.country = country
+		self.sunrise = sunrise
+		self.sunset = sunset
+
 
 class CurrentWeatherRespond:
 	def __init__(
