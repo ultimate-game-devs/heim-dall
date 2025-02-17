f06312f2028ae3e9a6d5b51593bf1d9e78578d57
Dominik <dasdodomit@gmail.com>
2025-02-17 22:21:45 +0100

    Try to display text

--- display.py
+++ display.py
@@ -10,33 +10,26 @@ def ILI9341():
 
 
 def SSD1306():
-	# Basic example of clearing and drawing pixels on a SSD1306 OLED display.
-	# This example and library is meant to work with Adafruit CircuitPython API.
-	# Author: Tony DiCola
-	# License: Public Domain
-
-	# Create the I2C interface.
 	i2c = busio.I2C(board.SCL, board.SDA)
-
-	# Create the SSD1306 OLED class.
-	# The first two parameters are the pixel width and pixel height.  Change these
-	# to the right size for your display!
 	display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 	# Alternatively you can change the I2C address of the device with an addr parameter:
 	# display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x31)
 
-	# Clear the display.  Always call show after changing pixels to make the display
+	# Clear the display. Always call show after changing pixels to make the display
 	# update visible!
 	display.fill(0)
 
 	display.show()
 
 	# Set a pixel in the origin 0,0 position.
-	display.pixel(0, 0, 1)
+	# display.pixel(0, 0, 1)
+	display.pixel("Zeile 1", 0, 0, 1)
+	display.pixel("Zeile 2", 0, 10, 1)
+	display.pixel("Zeile 3", 0, 20, 1)
 	# Set a pixel in the middle 64, 16 position.
-	display.pixel(64, 16, 1)
-	# Set a pixel in the opposite 127, 31 position.
-	display.pixel(127, 31, 1)
+	# display.pixel(64, 16, 1)
+	# # Set a pixel in the opposite 127, 31 position.
+	# display.pixel(127, 31, 1)
 	display.show()
 
 
