259b9ed4f5c4d4e51ba22f4573121af790d51bc1
Dominik <dasdodomit@gmail.com>
2025-02-17 12:33:34 +0100

    Formatting changes Checking the optional cases Parsed everything into weather_response

--- weatherAPI.py
+++ weatherAPI.py
@@ -1,9 +1,21 @@
 # First try
-import requests
 import os
+
+import requests
 from dotenv import load_dotenv
+
 from Geolocation import GeolocationResult, Location
-from Weather import Coord, Weather, Main, Wind, Rain, Snow, Clouds, Sys, CurrentWeatherRespond
+from Weather import (
+	Coord,
+	Weather,
+	Main,
+	Wind,
+	Rain,
+	Snow,
+	Clouds,
+	Sys,
+	CurrentWeatherRespond,
+)
 
 load_dotenv()
 
@@ -14,67 +26,78 @@ weather_key = os.getenv('WEATHER_KEY')
 google_result = requests.post(
 	f'https://www.googleapis.com/geolocation/v1/geolocate?key={google_key}',
 ).json()
+print(google_result)
+
 location = Location(google_result['location']['lat'], google_result['location']['lng'])
 geolocation = GeolocationResult(google_result['accuracy'], location)
 
+# Current Weather API
 
 weather_result = requests.post(
 	f'https://api.openweathermap.org/data/2.5/weather?lat={geolocation.location.lat}&lon={geolocation.location.lng}&appid={weather_key}&units=metric&lang=de'
 ).json()
-
-coor = Coord(
-    weather_result['coord']['lon'], 
-    weather_result['coord']['lat']
-)
-
-# Print the weather_result to debug the structure
 print(weather_result)
 
-# Extract weather information with error handling
-try:
-    weather = Weather(
-        weather_result['weather'][0]['id'],  # Corrected key name
-        weather_result['weather'][0]['main'],
-        weather_result['weather'][0]['description'],
-        weather_result['weather'][0]['icon']
-    )
-except KeyError as e:
-    print(f"KeyError: {e} is missing in the weather data")
-
+coord = Coord(weather_result['coord']['lon'], weather_result['coord']['lat'])
+weather = []
+for i in range(len(weather_result['weather'])):
+	weather.append(
+		Weather(
+			weather_result['weather'][0]['id'],
+			weather_result['weather'][0]['main'],
+			weather_result['weather'][0]['description'],
+			weather_result['weather'][0]['icon'],
+		)
+	)
 main = Main(
 	weather_result['main']['temp'],
-    weather_result['main']['feels_like'],
-    weather_result['main']['temp_min'],
-    weather_result['main']['temp_max'],
+	weather_result['main']['feels_like'],
+	weather_result['main']['temp_min'],
+	weather_result['main']['temp_max'],
 	weather_result['main']['pressure'],
 	weather_result['main']['humidity'],
 	weather_result['main']['sea_level'],
-	weather_result['main']['grnd_level']
+	weather_result['main']['grnd_level'],
 )
-wind = Wind(
-	weather_result['wind']['speed'],
-	weather_result['wind']['deg'],
-	weather_result['wind']['gust']
-)
-rain =Rain(
-	weather_result['rain']['1h']
-)
-snow = Snow(
-	weather_result['snow']['1h']
-)
-clouds = Clouds(
-	weather_result['clouds']['cloudiness']
-)
+
+if 'gust' in weather_result['wind']:
+	wind = Wind(
+		weather_result['wind']['speed'],
+		weather_result['wind']['deg'],
+		weather_result['wind']['gust'],
+	)
+else:
+	wind = Wind(weather_result['wind']['speed'], weather_result['wind']['deg'])
+
+spezial_weather = None
+if 'rain' in weather_result:
+	spezial_weather = Rain(weather_result['rain']['1h'])
+elif 'snow' in weather_result:
+	spezial_weather = Snow(weather_result['snow']['1h'])
+
+clouds = Clouds(weather_result['clouds']['all'])
 sys = Sys(
-    weather_result['sys']['sys_type'],
-    weather_result['sys']['sys_id'],
-    weather_result['sys']['country'],
+	weather_result['sys']['type'],
+	weather_result['sys']['id'],
+	weather_result['sys']['country'],
 	weather_result['sys']['sunrise'],
-    weather_result['sys']['sunset']    
+	weather_result['sys']['sunset'],
 )
-weather_desciption = CurrentWeatherRespond(
-	
+weather_response = CurrentWeatherRespond(
+	coord=coord,
+	weather=weather,
+	base=weather_result['base'],
+	main=main,
+	visibility=weather_result['visibility'],
+	wind=wind,
+	spezial_weather=spezial_weather,
+	clouds=clouds,
+	dt=weather_result['dt'],
+	sys=sys,
+	timezone=weather_result['timezone'],
+	city_id=weather_result['id'],
+	name=weather_result['name'],
+	cod=weather_result['cod'],
 )
 
-
-print(weather_result)
+print(weather_response)
