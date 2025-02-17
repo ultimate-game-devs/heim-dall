a76b39cacae4a6b05be37ec2dcd91eae67750751
Dominik <dasdodomit@gmail.com>
2025-02-17 15:41:35 +0100

    Added __init__.py's to indicate packages Changed the import to account for packages

--- tests/button.py
+++ tests/button.py
@@ -1,6 +1,6 @@
 import time
 
-from inputDevices import Button
+from ..inputDevices import Button
 
 button = Button(23)
 
@@ -8,21 +8,21 @@ previous_state = button.get_data()
 last_change_time = time.time()
 
 while True:
-	current_state = button.get_data()
-	current_time = time.time()
+    current_state = button.get_data()
+    current_time = time.time()
 
-	time_diff = current_time - last_change_time
+    time_diff = current_time - last_change_time
 
-	if current_state != previous_state:
-		timestamp = time.strftime('%H:%M:%S', time.localtime(current_time))
+    if current_state != previous_state:
+        timestamp = time.strftime('%H:%M:%S', time.localtime(current_time))
 
-		state_str = 'Button Pressed'
-		if current_state:
-			'Button no longer pressed'
+        state_str = 'Button Pressed'
+        if current_state:
+            'Button no longer pressed'
 
-		print(f'{timestamp} - {state_str} (Δ {time_diff:.2f} seconds)')
+        print(f'{timestamp} - {state_str} (Δ {time_diff:.2f} seconds)')
 
-		last_change_time = current_time
-		previous_state = current_state
+        last_change_time = current_time
+        previous_state = current_state
 
-		time.sleep(0.2)
+        time.sleep(0.2)
--- tests/testTempAndHumid.py
+++ tests/testTempAndHumid.py
@@ -1,13 +1,13 @@
 from time import localtime
 
-from inputDevices import DHT11
+from ..inputDevices import DHT11
 
 while True:
-	now = localtime()
-	print(f'~~~ {now.tm_hour}:{now.tm_min}:{now.tm_sec} ~~~')
-	dht = DHT11(4)
-	dht_data = dht.get_data()
-	if dht_data['temperature'] is not None and dht_data['humidity'] is not None:
-		print(f'Temp - {dht_data["temperature"]}°C | Humid - {dht_data["humidity"]}%')
-	else:
-		raise 'Error: Temperature or Huminity None'
+    now = localtime()
+    print(f'~~~ {now.tm_hour}:{now.tm_min}:{now.tm_sec} ~~~')
+    dht = DHT11(4)
+    dht_data = dht.get_data()
+    if dht_data['temperature'] is not None and dht_data['humidity'] is not None:
+        print(f'Temp - {dht_data["temperature"]}°C | Humid - {dht_data["humidity"]}%')
+    else:
+        raise 'Error: Temperature or Huminity None'
