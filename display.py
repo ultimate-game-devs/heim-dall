3704d0ec5dfb1867d7c1a88bd5ea123e0d157a35
Dominik <dasdodomit@gmail.com>
2025-02-18 12:48:21 +0100

    Trying to get a dynamic font size

--- display.py
+++ display.py
@@ -8,89 +8,93 @@ import busio
 import digitalio
 from PIL import Image, ImageDraw, ImageFont
 
-FONT_PATH = 'fonts/Roboto-Regular.ttf'
-FONT_SIZE = 20
-
 
 def ili9341() -> None:
-		pass
+	pass
 
 
 def ssd1306(cords: list[tuple[int | Any, int | Any]]) -> None:
-		i2c = busio.I2C(board.SCL, board.SDA)
-		display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
-		# Alternatively you can change the I2C address of the device with an addr parameter:
-		# display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x31)
+	i2c = busio.I2C(board.SCL, board.SDA)
+	display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+	# Alternatively you can change the I2C address of the device with an addr parameter:
+	# display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x31)
 
-		# Clear the display. Always call show after changing pixels to make the display
-		# update visible!
-		display.fill(0)
+	# Clear the display. Always call show after changing pixels to make the display
+	# update visible!
+	display.fill(0)
 
-		display.show()
+	display.show()
 
-		# Set a pixel in the origin 0,0 position.
-		# display.pixel(0, 0, 1)
-		# Set a pixel in the middle 64, 16 position.
-		# display.pixel(64, 16, 1)
-		# # Set a pixel in the opposite 127, 31 position.
-		# display.pixel(127, 31, 1)
+	# Set a pixel in the origin 0,0 position.
+	display.pixel(0, 0, 1)
+	# Set a pixel in the middle 64, 16 position.
+	display.pixel(64, 16, 1)
+	# # Set a pixel in the opposite 127, 31 position.
+	display.pixel(127, 31, 1)
 
-		for i in range(len(cords)):
-				display.pixel(cords[i][0], cords[i][1], 1)
+	# for i in range(len(cords)):
+	# 		display.pixel(cords[i][0], cords[i][1], 1)
 
-		display.show()
+	display.show()
 
 
 def lcd() -> None:
-		lcd_rs = digitalio.DigitalInOut(board.D26)
-		lcd_en = digitalio.DigitalInOut(board.D19)
-		lcd_d7 = digitalio.DigitalInOut(board.D27)
-		lcd_d6 = digitalio.DigitalInOut(board.D22)
-		lcd_d5 = digitalio.DigitalInOut(board.D24)
-		lcd_d4 = digitalio.DigitalInOut(board.D25)
+	lcd_rs = digitalio.DigitalInOut(board.D26)
+	lcd_en = digitalio.DigitalInOut(board.D19)
+	lcd_d7 = digitalio.DigitalInOut(board.D27)
+	lcd_d6 = digitalio.DigitalInOut(board.D22)
+	lcd_d5 = digitalio.DigitalInOut(board.D24)
+	lcd_d4 = digitalio.DigitalInOut(board.D25)
 
-		lcd_columns = 16
-		lcd_rows = 2
+	lcd_columns = 16
+	lcd_rows = 2
 
-		lcd = character_lcd.Character_LCD_Mono(
-				lcd_rs,
-				lcd_en,
-				lcd_d4,
-				lcd_d5,
-				lcd_d6,
-				lcd_d7,
-				lcd_columns,
-				lcd_rows,
-		)
+	lcd = character_lcd.Character_LCD_Mono(
+		lcd_rs,
+		lcd_en,
+		lcd_d4,
+		lcd_d5,
+		lcd_d6,
+		lcd_d7,
+		lcd_columns,
+		lcd_rows,
+	)
 
-		lcd.message = 'Hello\nCircuitPython'
+	lcd.message = 'Hello\nCircuitPython'
 
 
-def text_to_pixel_coordinates(
-				text: str, offset: tuple[int, int] = (0, 0)
-) -> list[tuple[int, int]]:
+def text_to_pixel_coordinates(text: str) -> list[tuple[int, int]]:
+	font_height = 1000
+	font_width = 1000
+	font_path = 'fonts/Roboto-Regular.ttf'
+	font_size = 20
+
+	while font_width > 127 or font_height > 31:
 		try:
-				font = ImageFont.truetype(FONT_PATH, FONT_SIZE)
+			font = ImageFont.truetype(font_path, font_size)
 		except IOError:
-				raise IOError(f'Font file not found at path: {FONT_PATH}')
+			raise IOError(f'Font file not found at path: {font_path}')
 
 		bbox = font.getbbox(text)
-		width = int(round(bbox[2] - bbox[0], 0))
-		height = int(round(bbox[3] - bbox[1], 0))
+		font_width = bbox[2] - bbox[0]
+		font_height = bbox[3] - bbox[1]
+		print(font_width, font_height)
+		if font_width > 127 or font_height > 31:
+			font_size -= 1
 
-		image = Image.new('L', (width, height))
-		draw = ImageDraw.Draw(image)
-		draw.text((0, 0), text, fill=255, font=font)
+	image = Image.new('L', (127, 31))
+	draw = ImageDraw.Draw(image)
+	draw.text((0, 0), text, fill=255, font=font)
 
-		image_width, image_height = image.size
+	image_width, image_height = image.size
 
-		cords = []
-		for y in range(image_height):
-				for x in range(image_width):
-						if x > 128:
-								print((x + offset[0], y + offset[1]))
-								cords.append((x + offset[0], y + offset[1]))
-		return cords
+	cords = []
+	for y in range(image_height):
+		for x in range(image_width):
+			if x > 128:
+				print((x, y))
+				cords.append((x, y))
+	return cords
 
 
 hostname = socket.gethostname()
