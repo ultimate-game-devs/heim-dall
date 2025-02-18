32244f02147ecc791e6e75657ec85b186260d24a
Dominik <dasdodomit@gmail.com>
2025-02-18 16:15:49 +0100

    Only relly on input no more default

--- display.py
+++ display.py
@@ -1,24 +1,8 @@
-import socket
-
-import psutil
-
 from helper.outputDevices import SSD1306
 
 display = SSD1306()
 
 text = ''
 
-stats = psutil.net_if_stats()
-addresses = psutil.net_if_addrs()
-
-for iface, stat in stats.items():
-	if stat.isup:
-		for snic in addresses.get(iface, []):
-			if snic.family == socket.AF_INET:
-				text = snic.address
-
-newText = input()
-if not newText == '\n' or not newText == '' or not newText == ' ':
-	text = newText
-	
-display.print_on_display(text)
+while True:
+	display.print_on_display(input())
