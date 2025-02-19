d3ad5c723435e3bd982102465381783ca0bfb57e
Dominik <dasdodomit@gmail.com>
2025-02-19 09:17:25 +0100

    Publishing the temp and the humd into a topic Also trying to reconnect when connection is lost

---
+++ secondary_PI.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+from helper.inputDevices import DHT22
+from helper.mqtt import MQTT
+
+client = MQTT('10.103.48.1', 'secondary_PI')
+dht = DHT22(4)
+
+temperature: float = 10000000
+humidity: float = 10000000
+
+while True:
+	data = dht.get_data()
+	if temperature != data['temperature']:
+		client.publish('outside/temperature', data['temperature'])
+		temperature = data['temperature']
+	if humidity != data['humidity']:
+		client.publish('outside/humidity', data['humidity'])
+		humidity = data['humidity']
+
+	while not client.check_connection():
+		client.reconnect()
+	sleep(0.1)
